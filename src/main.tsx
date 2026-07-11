1514180315d5d4a456005fe7d107b3cf4d936e72
Fernn <76133413+fernnje@users.noreply.github.com>
2026-07-11 13:43:24 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,4 +8,3 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>
 )
-<script type='text/javascript' src='https://edge-cdn.trakteer.id/js/trbtn-overlay.min.js?v=14-05-2025'></script><script type='text/javascript' class='troverlay'>(function() {var trbtnId = trbtnOverlay.init('Support Us','#6C5CE7','https://trakteer.id/v1/fernn/tip/embed/modal','https://edge-cdn.trakteer.id/images/embed/trbtn-icon.png?v=14-05-2025','40','floating-right');trbtnOverlay.draw(trbtnId);})();</script>
