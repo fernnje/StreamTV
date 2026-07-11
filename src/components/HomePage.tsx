e2696c515880fcdd837b2e27ae75668143fde4bd
Fernn <76133413+fernnje@users.noreply.github.com>
2026-07-11 13:43:58 +0700

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -284,6 +284,7 @@ export default function HomePage({ onNavigate }: HomePageProps) {
           <ArrowRight className="w-4 h-4 shrink-0 text-accent-light" />
         </button>
       </section>
+      <script type='text/javascript' src='https://edge-cdn.trakteer.id/js/trbtn-overlay.min.js?v=14-05-2025'></script><script type='text/javascript' class='troverlay'>(function() {var trbtnId = trbtnOverlay.init('Support Us','#6C5CE7','https://trakteer.id/v1/fernn/tip/embed/modal','https://edge-cdn.trakteer.id/images/embed/trbtn-icon.png?v=14-05-2025','40','floating-right');trbtnOverlay.draw(trbtnId);})();</script>
     </div>
   )
 }
